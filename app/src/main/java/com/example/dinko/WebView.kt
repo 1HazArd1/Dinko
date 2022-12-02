52be105179f085bf188aea9af75970883575ca2a
1HazArd1 <aakashgoingto99@gmail.com>
2022-12-03 02:14:25 +0530

    Implemented Web View

--- app/src/main/java/com/example/dinko/WebView.kt
+++ app/src/main/java/com/example/dinko/WebView.kt
@@ -2,10 +2,29 @@ package com.example.dinko
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.activity_web_view.*
 
 class WebView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web_view)
+
+        val webUrl=intent.getStringExtra("url").toString()
+        webView.webViewClient=WebViewClient()
+        webView.loadUrl(webUrl)
+        webView.settings.setSupportZoom(true)
+    }
+    // if you press Back button this code will work
+    override fun onBackPressed() {
+        // if your web view can go back it will go back
+        if (webView.canGoBack())
+            webView.goBack()
+        // if your web view cannot go back
+        // it will exit the application
+        else
+            super.onBackPressed()
     }
 }
+
+
